f3c2e7c098085b9884bdeb9f5920bedf77554321
OzgeBahceci <ozge.bahceci@hotmail.com>
2023-11-26 23:43:36 +0300

    Ürünlerin listelenmesi

--- FormsApp/Models/Product.cs
+++ FormsApp/Models/Product.cs
@@ -1,12 +1,19 @@
-﻿namespace FormsApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models
 {
     public class Product
     {
+        //İsmini çekerken görünecek olan isim
+        [Display(Name = "Ürün Id")]
         public int ProductId { get; set; }
 
         //public string? Name { get; set; } ile aynı
-        public string Name { get; set; } = string.Empty; 
+        [Display(Name = "Ürün Adı")]
+        public string Name { get; set; } = string.Empty;
+        [Display(Name = "Fiyatı")]
         public decimal Price { get; set; }
+        [Display(Name = "Resim")]
         public string? Image { get; set; }
         public bool IsActive { get; set; }
 
--- FormsApp/Models/Repository.cs
+++ FormsApp/Models/Repository.cs
@@ -8,6 +8,16 @@
 
         static Repository()
         {
+            _categories.Add(new Category { CategoryId = 1, Name = "Telefon" });
+            _categories.Add(new Category { CategoryId = 2, Name = "Bilgisayar" });
+
+            _products.Add(new Product { ProductId = 1, Name = "Iphone 14", Price = 40000, IsActive = true, Image = "1.jpg", CategoryId = 1 });
+            _products.Add(new Product { ProductId = 2, Name = "Iphone 15", Price = 50000, IsActive = true, Image = "2.jpg", CategoryId = 1 });
+            _products.Add(new Product { ProductId = 3, Name = "Iphone 16", Price = 60000, IsActive = true, Image = "3.jpg", CategoryId = 1 });
+            _products.Add(new Product { ProductId = 4, Name = "Iphone 17", Price = 70000, IsActive = true, Image = "4.jpg", CategoryId = 1 });
+           
+            _products.Add(new Product { ProductId = 5, Name = "Macbook Air", Price = 80000, IsActive = true, Image = "5.jpg", CategoryId = 2 });
+            _products.Add(new Product { ProductId = 6, Name = "Macbook Pro", Price = 70000, IsActive = true, Image = "6.jpg", CategoryId = 2 });
 
         }
 
