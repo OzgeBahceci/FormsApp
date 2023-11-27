c3d9173070c87d919be8690106399dad43c9ee9d
ozgebahceci <ozge.bahceci@hotmail.com>
2023-11-27 10:48:07 +0300

    ProductViewModelden gelen ekrana basma hatası giderildi

--- FormsApp/Models/ProductViewModel.cs
+++ FormsApp/Models/ProductViewModel.cs
@@ -7,7 +7,7 @@
         //null değil
         public List<Product> Products { get; set; } = null!;
         public List<Category> Categories { get; set; } = null!;
-        public string SelectedCategory { get; set; }
+        public string? SelectedCategory { get; set; }
 
     }
 }
